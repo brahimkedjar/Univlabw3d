a1e7f22b679cd5f2b60f6e3524da8513b2f994c8
BrahimKedjar <jemskedjar@gmail.com>
2022-11-11 21:23:14 +0100

    database added

---
+++ database/migrations/2022_11_11_193551_steps.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('steps', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('tp_id');
+            $table->longText('step');
+            $table->timestamps();
+            $table->foreign('tp_id')->references('id')->on('tps');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('steps');
+    }
+};
--- database/migrations/2022_11_11_193634_type_3dobjects.php
+++ database/migrations/2022_11_11_193634_type_3dobjects.php
@@ -13,13 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+         Schema::create('type_3dObjects', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->enum('type', ['Scene', 'Produit', 'Appareil']);
             $table->timestamps();
         });
     }
@@ -31,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('type_3dObjects');
     }
 };
