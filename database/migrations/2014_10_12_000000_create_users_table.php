eafedd1fc7f592b49c493aaf22c048642575f3d5
Simango Psychology <psicologia5086@gmail.com>
2022-11-14 16:33:56 +0100

    first commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,32 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->unsignedBigInteger('role_id')->nullable();
+    $table->unsignedBigInteger('speciality_id')->nullable();
+            $table->unsignedBigInteger('group_id')->nullable();
+            $table->string('username', 50)->unique();
+            $table->string('password')->unique()->nullable()->default('00000000');
+            $table->integer('inscription_number')->nullable()->unique();
+            $table->string('first_name',50)->nullable();
+            $table->string('last_name',50)->nullable();
+            $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('set null')
+                ->onUpdate('cascade');
+            $table->foreign('speciality_id')
+                ->references('id')
+                ->on('specialities')
+                ->onDelete('set null')
+                ->onUpdate('cascade');
+            $table->foreign('group_id')
+                ->references('id')
+                ->on('groups')
+                ->onDelete('set null')
+                ->onUpdate('cascade');
         });
     }
 
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+//Public Routes
+Route::post('/login', [AuthController::class,'login']);
+Route::post('/register', [AuthController::class,'register']);
+//Protected routes
+
+Route::group(['middleware'=>['auth:sanctum']], function(){
+    Route::post('/logout',[AuthController::class, 'logout']);
 });
