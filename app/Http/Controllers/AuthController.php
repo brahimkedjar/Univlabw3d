f1a75d12a9778f995b157e9d147763e071d501a2
Simango Psychology <psicologia5086@gmail.com>
2022-11-30 15:02:31 +0100

    modified

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -47,7 +47,7 @@ class AuthController extends Controller
     }
 
     public function logout(){
-    Auth::user()->currentAccessToken()->delete();
+    User::user()->currentAccessToken()->delete();
     return $this->success([
         'message' => 'Logout Successful, permissions revoked'
     ]
--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -9,9 +9,18 @@ use Illuminate\Http\Request;
 
 class ModuleController extends Controller
 {
-
-
-    use HttpResponses;
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($request)
+    {
+        $modules = Module::where('');
+        return $this->success([
+            'modules'=>$modules,
+        ]);
+    }
     public function store(StoreModuleRequest $request)
     {
         $request->validated($request->all());
@@ -27,5 +36,40 @@ class ModuleController extends Controller
         ]);
     }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $module = Module::find($id);
+        $module->update($request);
+        return $module;
+    }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $module = Module::destroy($id);
+        return $this->success([
+            'message'=>'deleted successfull',
+        ]);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Module::find($id);
+    }
 }
